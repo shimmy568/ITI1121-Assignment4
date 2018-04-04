8f3eb70e1fca4fcee26888d7dfb45fa9f46a52f9
Owen Anderson <owenthomasanderson@gmail.com>
2018-04-04 07:11:48 -0400

    Added files for Q4

---
+++ src/main/java/BinarySearchTree.java
@@ -0,0 +1,89 @@
+/**
+*  @author Marcel Turcotte
+*/
+
+import java.util.NoSuchElementException;
+
+public class BinarySearchTree<E extends Comparable<E>> {
+
+    private static class Node<T> {
+        private T value;
+        private Node<T> left;
+        private Node<T> right;
+        private Node(T value) {
+            this.value = value;
+            left = null;
+            right = null;
+        }
+    }
+
+    private Node<E> root = null;
+
+    /**
+    * Inserts an object into this BinarySearchTree.
+    *
+    * @param elem item to be added
+    * @return true if the object has been added and false otherwise
+    */
+
+    public boolean add(E elem) {
+
+        // pre-condtion:
+
+        if (elem == null) {
+            throw new NullPointerException();
+        }
+
+        // special case:
+
+        if (root == null) {
+            root = new Node<E>(elem);
+            return true;
+        }
+
+        // general case:
+
+        return add(elem, root);
+    }
+
+    // Helper method
+
+    private boolean add(E elem, Node<E> current) {
+
+        boolean result;
+        int test = elem.compareTo(current.value);
+
+        if (test == 0) {
+            result = false; // already exists, not added
+        } else if (test < 0) {
+            if (current.left == null) {
+                current.left = new Node<E>(elem);
+                result = true;
+            } else {
+                result = add(elem, current.left);
+            }
+        } else {
+            if (current.right == null) {
+                current.right = new Node<E>(elem);
+                result = true;
+            } else {
+                result = add(elem, current.right);
+            }
+        }
+        return result;
+    }
+
+    public String toString() {
+        return toString(root);
+    }
+
+    private String toString(Node<E> current) {
+
+        if (current == null) {
+            return "()";
+        }
+
+        return "(" + toString(current.left) + current.value + toString(current.right) + ")";
+    }
+
+}
